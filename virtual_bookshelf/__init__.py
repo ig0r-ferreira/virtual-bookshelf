4b1cfa4f4a181aaf9133cd18648b61674e5028f5
Igor Ferreira <iferreira.n01@gmail.com>
2023-03-13 20:02:34 -0300

    Add homepage

---
+++ virtual_bookshelf/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+
+@app.route('/')
+def home() -> str:
+    return render_template('index.html')
