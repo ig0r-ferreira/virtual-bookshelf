8b261e13ac6e6b33fcc8710421ca105f76bdef6c
Igor Ferreira <iferreira.n01@gmail.com>
2023-03-13 21:11:43 -0300

    Add page to add a new book

--- virtual_bookshelf/__init__.py
+++ virtual_bookshelf/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template
+from flask import Flask, redirect, render_template, request, url_for
 from flask_bootstrap import Bootstrap5
+from werkzeug.wrappers import Response
 
 app = Flask(__name__)
 bootstrap = Bootstrap5(app)
@@ -8,3 +9,11 @@ bootstrap = Bootstrap5(app)
 @app.route('/')
 def home() -> str:
     return render_template('index.html')
+
+
+@app.route('/add', methods=['GET', 'POST'])
+def add_book() -> str | Response:
+    if request.method == 'POST':
+        return redirect(url_for('home'))
+
+    return render_template('add.html')
