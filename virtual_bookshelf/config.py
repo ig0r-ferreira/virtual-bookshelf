6bcfde21c0ef9ec8d906fd15ce209f44eaa4ba8c
Igor Ferreira <iferreira.n01@gmail.com>
2023-05-11 18:12:06 -0300

    Make the path to the database fixed

--- virtual_bookshelf/config.py
+++ virtual_bookshelf/config.py
@@ -1,3 +1,5 @@
+import pathlib
+
 from dotenv import load_dotenv
 from flask import Flask
 
@@ -5,6 +7,8 @@ load_dotenv()
 
 
 def init_app(app: Flask, **config) -> None:
+    pathlib.Path(app.instance_path).mkdir(exist_ok=True)
+    app.config['DATABASE_URI'] = 'sqlite:///instance/bookshelf.db'
     app.config.from_prefixed_env()
     if config:
         app.config.from_mapping(config)
